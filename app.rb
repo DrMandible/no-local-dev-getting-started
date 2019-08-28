00c8d011a0653de6e851ae800310f5a97e6278b8
DrMandible <46269701+DrMandible@users.noreply.github.com>
2019-08-28 13:59:20 -0500

    Update app.rb

--- app.rb
+++ app.rb
@@ -10,14 +10,14 @@ get "/" do
 end
 
 
-class Contact < ActiveRecord::Base
-  self.table_name = 'salesforce.contact'
+class LeaseMods < ActiveRecord::Base
+  self.table_name = 'salesforce.Lease_Modification__c'
 end
 
-#get "/contacts" do
-#  @contacts = Contact.all
-#  erb :index
-#end
+get "/leasemods" do
+  @leasemods = LeaseMods.all
+  erb :index
+end
 
 get "/create" do
   dashboard_url = 'https://dashboard.heroku.com/'
